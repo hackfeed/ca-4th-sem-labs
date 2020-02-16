bc078d527ffeb66febc3910bfce7f0633f2bd73d
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-16 22:40:15 +0300

    Add interpolation table creation

--- lab_01/src/interpolation/interpolation.go
+++ lab_01/src/interpolation/interpolation.go
@@ -6,19 +6,43 @@ import (
 	"io"
 )
 
+func MakeTable(ds cartesian.DotSet, d cartesian.Dot, n int) [][]float64 {
+	base := GetBase(ds, d, n)
+	baselen := len(base)
+	tb := make([][]float64, n+2)
+	tblen := len(tb)
+	for i := range tb {
+		tb[i] = make([]float64, baselen)
+	}
+
+	for i := range tb[0] {
+		tb[0][i] = base[i].X
+		tb[1][i] = base[i].Y
+	}
+
+	for i := 2; i < tblen; i++ {
+		k := i - 2
+		for j := 0; j < baselen-i+1; j++ {
+			tb[i][j] = (tb[i-1][j] - tb[i-1][j+1]) / (tb[0][j] - tb[0][j+k+1])
+		}
+	}
+
+	return tb
+}
+
 func GetBase(ds cartesian.DotSet, d cartesian.Dot, n int) cartesian.DotSet {
 	base := cartesian.DotSet{}
 	pos := ds.GetPos(d)
 
 	if pos < n/2 {
-		for i := 0; i < n+1; i++ {
+		for i := 0; i < n+2; i++ {
 			if i == pos {
 				continue
 			}
 			base.Append(ds[i])
 		}
 	} else if len(ds)-pos-1 < n/2 {
-		for i := len(ds) - n - 1; i < len(ds); i++ {
+		for i := len(ds) - n - 2; i < len(ds); i++ {
 			if i == pos {
 				continue
 			}
@@ -26,7 +50,7 @@ func GetBase(ds cartesian.DotSet, d cartesian.Dot, n int) cartesian.DotSet {
 		}
 	} else {
 		lb := n / 2
-		rb := n - lb + 1
+		rb := n - lb + 2
 
 		for i := pos - lb; i < pos+rb; i++ {
 			if i == pos {
