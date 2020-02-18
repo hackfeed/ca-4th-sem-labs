ec6f6fe60c011d2fbb02c0e1adf588ac59411c3e
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-18 19:34:19 +0300

    Add CLI arguments usage

--- lab_01/main.go
+++ lab_01/main.go
@@ -9,11 +9,19 @@ import (
 )
 
 func main() {
-	f, err := os.Open("data/dots.txt")
+	if len(os.Args) <= 1 {
+		fmt.Printf("USAGE: lab_01 <datafile>\n")
+		os.Exit(1)
+	}
+
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+
+	eps := 0.00001
+	bisErr := -1.111
 
 	ds := interp.ReadDots(f)
 	fmt.Printf("Table loaded from file:\n\n")
@@ -25,12 +33,14 @@ func main() {
 	sort.Sort(ds)
 
 	d.Y = interp.Interpolation(ds, d, n)
-	fmt.Printf("Function value in %5.2f dot is %5.4f\n\n", d.X, d.Y)
+	fmt.Printf("\nFunction value in %5.2f dot is %5.4f\n\n", d.X, d.Y)
 
-	rootBis := interp.Bisection(ds, n, 0.001)
-	fmt.Printf("Root found by bisection method is %.4f\n", rootBis)
-	fmt.Printf("If root value is equivalent to -1.1110, that means that\n" +
-		"there is no possibility to find root by bisection method in given dot set.\n\n")
+	rootBis := interp.Bisection(ds, n, eps)
+	if rootBis-bisErr < eps {
+		fmt.Printf("Can't find root by bisection method in given dot set\n\n")
+	} else {
+		fmt.Printf("Root found by bisection method is %.4f\n", rootBis)
+	}
 
 	rootInvInterp := interp.InvInterpolation(ds, n)
 	fmt.Printf("Root found by inverted interpolation method is %.4f\n\n", rootInvInterp)
